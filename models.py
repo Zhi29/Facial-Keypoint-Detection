271327fe753825e92458966c7bf3cadd3c673421
Zhi Li <42852591+Zhi29@users.noreply.github.com>
2020-02-21 19:59:21 -0800

    initial upload

---
+++ models.py
@@ -0,0 +1,75 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # 224x224x1 grayscale input image
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+
+
+        self.conv1 = nn.Conv2d(1, 32, 4)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, 2)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(128, 256, 1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.maxpool = nn.MaxPool2d(2,2) 
+
+        self.fc1_drop = nn.Dropout(p=0.4)
+
+        self.fc1 = nn.Linear(13*13*256, 1000)
+        self.fc2 = nn.Linear(1000, 68*2)
+
+        self.dropout1 = nn.Dropout(p=0.1)
+        self.dropout2 = nn.Dropout(p=0.2)
+        self.dropout3 = nn.Dropout(p=0.3)
+        self.dropout4 = nn.Dropout(p=0.4)
+
+        #nn.init.xavier_uniform_(self.fc1.weight.data)
+        #nn.init.xavier_uniform_(self.fc2.weight.data)
+
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout1(self.maxpool(F.elu(self.bn1(self.conv1(x)))))
+        x = self.dropout2(self.maxpool(F.elu(self.bn2(self.conv2(x)))))
+        x = self.dropout3(self.maxpool(F.elu(self.bn3(self.conv3(x)))))
+        x = self.dropout4(self.maxpool(F.elu(self.bn4(self.conv4(x)))))
+
+        x = x.view(x.size(0), -1)
+        
+        x = F.elu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
+        
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
